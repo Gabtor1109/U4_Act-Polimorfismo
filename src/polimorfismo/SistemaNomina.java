d619a804dbcd4a2274120b039a862e75c05a386f
invitado <invitado@LAB010210.itchii.lab>
2022-04-29 12:54:39 -0600

    Agh

--- src/polimorfismo/SistemaNomina.java
+++ src/polimorfismo/SistemaNomina.java
@@ -23,7 +23,7 @@ public class SistemaNomina {
         EmpleadoPorComision empleadoPorComision =  new EmpleadoPorComision("Gabriel", " Torres", "12345", 200, 0.2);
         EmpleadoPorHoras empleadoPorHoras =  new EmpleadoPorHoras("Gabriel", " Torres", "12540", 200, 12);
         
-        System.out.println("Se procesa Empleados por separado");
+        System.out.println("////////////////////////////----- Se procesa Empleados por separado -----/////////////////////////////");
         System.out.printf("%s\n%s: $%,.2f\n\n",empleadoAsalariado,"Ingresos",empleadoAsalariado.ingresos());
         System.out.printf("%s\n%s: $%,.2f\n\n",empleadoBaseMasComision,"Ingresos",empleadoBaseMasComision.ingresos());
         System.out.printf("%s\n%s: $%,.2f\n\n",empleadoPorComision,"Ingresos",empleadoPorComision.ingresos());
@@ -31,16 +31,31 @@ public class SistemaNomina {
         
         
         Empleado []empleado = new Empleado[4];
-        empleado[0] = empleadoAsalariado;
-        empleado[1] = empleadoBaseMasComision;
+        empleado[0] = empleadoAsalariado;        empleado[1] = empleadoBaseMasComision;
         empleado[2] = empleadoPorComision;
         empleado[3] = empleadoPorHoras;
         
+        System.out.println("////////////////////////////----- Se procesa Empleados juntos -----////////////////////////////");
         for (int i = 0; i < empleado.length; i++) {
             System.out.printf("%s\n%s: $%,.2f\n\n",empleado[i],"Ingresos",empleado[i].ingresos());
         }
         
-        
+        System.out.println("///////////////////////----- Se procesa Empleados juntos (Metodo de la maestra) -----//////////////////////");
+        for(Empleado empleadoActual:empleado){
+            //Se invoca al metodo ToString(Me ayuda a obtener los valores d mi objeto)
+            System.out.println(empleadoActual);
+            if(empleadoActual instanceof EmpleadoBaseMasComision){
+            EmpleadoBaseMasComision empleados = (EmpleadoBaseMasComision) empleadoActual;
+            empleados.establecerSalarioBase(1.10*empleados.obtenerSalarioBase());
+            System.out.printf("El nuevo salario Base con 10%% de incremento es: $%,.2f\n",empleados.obtenerSalarioBase());
+            }
+            System.out.printf("Ingresos $%,.2f\n\n",empleadoActual.ingresos());
+        }
+        //Se crea un for para obtener el nombre del tipo de cada objeto en el arreglo empleados
+        System.out.println("Se obtiene el nombre del tipo de cada objeto en el arreglo");
+        for (int j = 0; j < empleado.length; j++) {
+            System.out.printf("El empleado %d es un %s\n",j,empleado[j].getClass().getName());            
+        }
         
        }
     }
